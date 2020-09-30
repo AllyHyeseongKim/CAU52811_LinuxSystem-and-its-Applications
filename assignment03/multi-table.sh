7fc9c42e4518d2a621a0d307221a8ee8eab05144
AllyHyeseongKim <ally3k@icloud.com>
2020-09-30 22:29:07 +0900

    [Assignment3] Update the input validation

--- assignment03/multi-table.sh
+++ assignment03/multi-table.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 row=$1
 col=$2
-for i in $(seq 1 $row)
-do
-	for j in $(seq 1 $col)
+if [ $row -ge 1 -a $col -ge 1 ]
+then
+	for i in $(seq 1 $row)
 	do
-		echo -n $i '*' $j '=' `expr $i \* $j` ' '
+		for j in $(seq 1 $col)
+		do
+			echo -n $i '*' $j '=' `expr $i \* $j` ' '
+		done
+		echo
 	done
-	echo
-done
+else
+	echo [Input Range Error] Row and col must be an integer greater than or equal to 1.
+fi
 exit 0
