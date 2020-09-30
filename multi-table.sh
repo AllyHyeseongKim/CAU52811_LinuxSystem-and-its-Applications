8d5f6c5171900ab8fbcf50e74e4b82d04c825229
AllyHyeseongKim <ally3k@icloud.com>
2020-09-30 22:01:19 +0900

    [Assignment3] Update multi-table function

--- multi-table.sh
+++ multi-table.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-echo "row: $1"
-echo "col: $2"
-for i in $(seq 1 $1)
+row=$1
+col=$2
+for i in $(seq 1 $row)
+do
+	for j in $(seq 1 $col)
+	do
+		echo -n $i '*' $j '=' `expr $i \* $j` ' '
+	done
+	echo
+done
 exit 0
